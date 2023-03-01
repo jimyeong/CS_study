43e4aa1bc6632c9ef0f37ddb8cafc2f45a5ce7c1
JIMMY JUNG <idjjm92@gmail.com>
2023-03-01 14:37:24 +0000

    init

--- tree/cloneBinaryTreeWithRandomPointer/Solution.cpp
+++ tree/cloneBinaryTreeWithRandomPointer/Solution.cpp
@@ -1,14 +1,14 @@
 
-
+// https://leetcode.com/problems/clone-binary-tree-with-random-pointer/description/
  struct Node {
-       int val;
-        Node *left;
- Node *right;
-      Node *random;
-      Node() : val(0), left(nullptr), right(nullptr), random(nullptr) {}
-      Node(int x) : val(x), left(nullptr), right(nullptr), random(nullptr) {}
-      Node(int x, Node *left, Node *right, Node *random) : val(x), left(left), right(right), random(random) {}
-  };
+    int val;
+    Node *left;
+    Node *right;
+    Node *random;
+    Node() : val(0), left(nullptr), right(nullptr), random(nullptr) {}
+    Node(int x) : val(x), left(nullptr), right(nullptr), random(nullptr) {}
+    Node(int x, Node *left, Node *right, Node *random) : val(x), left(left), right(right), random(random) {}
+};
  
 
 #include "unordered_map"

b2ae895cb6fd931220d5fedb1407bd22d42185b1
JIMMY JUNG <idjjm92@gmail.com>
2023-03-20 22:08:01 +0000

    disjoint set

---
+++ graph/disjoint/quickUnion/disjoint_quick_union.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class UnionFind{
+    private:
+    vector<int> _root; // shows connecting information with a node's parent
+
+    public:
+    int find(int node){
+        // O(n);
+        // the subject node needs to traverse through all connectivity to figure out the root of the node(subject);
+        while(_root[node] != node){
+            node = _root[node];
+        };
+        return node;
+    };
+
+    void unionSet(int subject_node, int target_node){
+        if(!isConnected(subject_node, target_node)){
+            _root[find(subject_node)] = target_node;
+        }
+    };
+
+    bool isConnected(int x, int y){
+        // if they have the same root node
+        return find(x) == find(y);
+    }
+
+    UnionFind(int size):_root(size){
+        for(int i=0; i < size; i ++){
+            _root[i] = i;
+        }
+    };
+};
+
+
+int main(){
+    UnionFind uf(10);
+    uf.unionSet(1, 2);
+    uf.unionSet(2, 5);
+    uf.unionSet(5, 6);
+    uf.unionSet(6, 7);
+    uf.unionSet(3, 8);
+    uf.unionSet(8, 9);
+    cout << uf.isConnected(1, 5) << endl;  // true
+    cout << uf.isConnected(5, 7) << endl;  // true
+    cout << uf.isConnected(4, 9) << endl;  // false
+    // 1-2-5-6-7 3-8-9-4
+    uf.unionSet(9, 4);
+    cout << uf.isConnected(4, 9) << endl;  // true
+
+    return 0;
+};
